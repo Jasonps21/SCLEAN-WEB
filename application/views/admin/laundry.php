f9722cb699ce7e1ff38c4fab59db620a163f1fd2
unknown <jpsunarji21@gmail.com>
2021-02-27 12:34:54 +0800

    hilangkan kolom rekomendasi di bagian pemilik laundry

--- application/views/admin/laundry.php
+++ application/views/admin/laundry.php
@@ -1,3 +1,4 @@
+<?php $isLoggedIn = $this->session->userdata('status'); ?>
 <div class="main-content-inner">
     <div class="row">
         <div class="col-12 mt-5">
@@ -38,7 +39,6 @@
                                     <th width="1%">No</th>
                                     <th width="8%">Gambar</th>
                                     <?php
-                                    $isLoggedIn = $this->session->userdata('status');
                                     if ($isLoggedIn === "Admin") {
                                     ?>
                                         <th>Pemilik</th>
@@ -48,7 +48,11 @@
                                     <th width="10%">Nomor Telepon</th>
                                     <th width="10%">Biaya Pengantaran</th>
                                     <th width="8%">Jam Buka</th>
-                                    <th width="5%">Rekomendasi</th>
+                                    <?php
+                                    if ($isLoggedIn === "Admin") {
+                                    ?>
+                                        <th width="5%">Rekomendasi</th>
+                                    <?php } ?>
                                     <th width="10%">Opsi</th>
                                 </tr>
                             </thead>
@@ -59,37 +63,50 @@
                                 ?>
                                     <tr>
                                         <td class="text-center"><?php echo $no++ ?></td>
-                                        <td class="text-center"><img src="<?php echo base_url() . "assets/images/produk/" . $u->photo ?>" width="200px" height="200px" </td>
-                                            <?php
-                                            $isLoggedIn = $this->session->userdata('status');
-                                            if ($isLoggedIn === "Admin") {
-                                            ?>
-                                        <td class="text-center"><?php echo $u->nama ?></td>
-                                    <?php } ?>
-                                    <td class="text-center"><?php echo $u->nama_laundry ?></td>
-                                    <td class="text-center"><?php echo $u->alamat ?></td>
-                                    <td class="text-center"><?php echo $u->nomor_telepon ?></td>
-                                    <td class="text-center"><?php echo $u->biaya_pengantaran ?></td>
-                                    <td class="text-center"><?php echo date_format(new DateTime($u->jam_buka), 'H:i') . ' - ' . date_format(new DateTime($u->jam_tutup), 'H:i'); ?></td>
-                                    <td class="text-center">
+                                        <td class="text-center"><img src="<?php echo base_url() . "assets/images/produk/" . $u->photo ?>" width="200px" height="200px"> </td>
                                         <?php
-                                        if ($u->is_recommend == 0) {
+                                        if ($isLoggedIn === "Admin") {
                                         ?>
-                                            <span class="badge badge-danger">Tidak</span>
+                                            <td class="text-center"><?php echo $u->nama ?></td>
+                                        <?php } ?>
+                                        <td class="text-center"><?php echo $u->nama_laundry ?></td>
+                                        <td class="text-center"><?php echo $u->alamat ?></td>
+                                        <td class="text-center"><?php echo $u->nomor_telepon ?></td>
+                                        <td class="text-center"><?php echo $u->biaya_pengantaran ?></td>
+                                        <td class="text-center"><?php echo date_format(new DateTime($u->jam_buka), 'H:i') . ' - ' . date_format(new DateTime($u->jam_tutup), 'H:i'); ?></td>
                                         <?php
-                                        } else if ($u->is_recommend == 1) {
+                                        if ($isLoggedIn === "Admin") {
                                         ?>
-                                            <span class="badge badge-success">Ya</span>
-                                        <?php
-                                        }
-                                        ?>
-                                    </td>
-                                    <td class="text-center">
-                                        <a class="btn btn-sm btn-warning" href="<?php echo base_url() . 'Laundry/edit_laundry/' . $u->id_laundry ?>"><i class="ti-pencil"></i></a>
-                                        <a class="btn btn-sm btn-danger" onclick="javascript: return confirm('Apakah Anda yakin ingin hapus data ini?')" href="<?php echo base_url() . 'Laundry/hapus_laundry/' . $u->id_laundry . '/' . $u->photo ?>"><i class="ti-trash"></i></a>
-                                        <a class="btn btn-sm btn-info" href="<?php echo base_url() . 'Laundry/daftar_layanan/' . $u->id_laundry ?>"><i class="ti-eye"></i></a>
-                                        <a class="btn btn-sm <?php if ($u->is_recommend == 0) { echo 'btn-primary';} else { echo 'btn-dark';} ?>" <?php if ($u->is_recommend == 0) { echo 'data-toggle="tooltip" data-placement="bottom" title="Set Rekomendasi"';} else { echo 'data-toggle="tooltip" data-placement="bottom" title="Batal Rekomendasi"';} ?> href="<?php echo base_url() . 'Laundry/laundry_set_recommend/' . $u->id_laundry ?>"><i class="ti-star"></i></a>
-                                    </td>                                    
+                                            <td class="text-center">
+                                                <?php
+                                                if ($u->is_recommend == 0) {
+                                                ?>
+                                                    <span class="badge badge-danger">Tidak</span>
+                                                <?php
+                                                } else if ($u->is_recommend == 1) {
+                                                ?>
+                                                    <span class="badge badge-success">Ya</span>
+                                                <?php
+                                                }
+                                                ?>
+                                            </td>
+                                        <?php } ?>
+                                        <td class="text-center">
+                                            <a class="btn btn-sm btn-warning" href="<?php echo base_url() . 'Laundry/edit_laundry/' . $u->id_laundry ?>"><i class="ti-pencil"></i></a>
+                                            <a class="btn btn-sm btn-danger" onclick="javascript: return confirm('Apakah Anda yakin ingin hapus data ini?')" href="<?php echo base_url() . 'Laundry/hapus_laundry/' . $u->id_laundry . '/' . $u->photo ?>"><i class="ti-trash"></i></a>
+                                            <a class="btn btn-sm btn-info" href="<?php echo base_url() . 'Laundry/daftar_layanan/' . $u->id_laundry ?>"><i class="ti-eye"></i></a>
+                                            <?php if ($isLoggedIn === "Admin") { ?>
+                                                <a class="btn btn-sm <?php if ($u->is_recommend == 0) {
+                                                                            echo 'btn-primary';
+                                                                        } else {
+                                                                            echo 'btn-dark';
+                                                                        } ?>" <?php if ($u->is_recommend == 0) {
+                                                                                echo 'data-toggle="tooltip" data-placement="bottom" title="Set Rekomendasi"';
+                                                                            } else {
+                                                                                echo 'data-toggle="tooltip" data-placement="bottom" title="Batal Rekomendasi"';
+                                                                            } ?> href="<?php echo base_url() . 'Laundry/laundry_set_recommend/' . $u->id_laundry ?>"><i class="ti-star"></i></a>
+                                            <?php } ?>
+                                        </td>
                                     </tr>
                                 <?php } ?>
                             </tbody>
